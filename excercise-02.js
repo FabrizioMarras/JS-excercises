3d4371ddbe0719ea1ca888998ade74e08f29dbf2
Fabrizio Marras <37974948+FabrizioMarras@users.noreply.github.com>
2019-11-29 13:20:06 +0100

    Update excercise-02.js

--- excercise-02.js
+++ excercise-02.js
@@ -9,9 +9,9 @@ for (let index = 1; index <= 100; index++) {
   if (index % 3 === 0) {
     console.log("Fizz");
   } else if (index % 5 === 0) {
-    console.log("Buzz")
+    console.log("Buzz");
   } else {
-    console.log(index)
+    console.log(index);
   }
 }
 
@@ -26,8 +26,8 @@ for (let index = 1; index <= 100; index++) {
   } else if (index % 3 === 0) {
     console.log("Fizz");
   } else if (index % 5 === 0) {
-    console.log("Buzz")
+    console.log("Buzz");
   } else {
-    console.log(index)
+    console.log(index);
   }
 }
