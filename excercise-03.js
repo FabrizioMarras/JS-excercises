f1a644b3874206fca767f164868026fa31e6c004
Fabrizio Marras <37974948+FabrizioMarras@users.noreply.github.com>
2019-11-29 14:00:53 +0100

    Update excercise-03.js

--- excercise-03.js
+++ excercise-03.js
@@ -4,4 +4,19 @@ at each position of the grid there is either a "" (space) or a "#" character.
 
 Pass then the string to the console to output the chess board-like grid.
 */
+let result = "";
+for (let y = 0; y < 8; y++) {
+  for (let x = 0; x < 8; x++) {      
+    (x + y) % 2 === 0 ? result += " " : result += "#";
+  }
+  result += "\n";
+}
 
+console.log(result);
+
+/*
+# # # #
+ # # # #
+# # # # 
+ # # # #
+*/
