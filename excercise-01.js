230628b0e9aab9ec59ce37b23573175a8dbe2fde
Fabrizio Marras <37974948+FabrizioMarras@users.noreply.github.com>
2019-11-29 11:14:02 +0100

    Create excercise-01.js

---
+++ excercise-01.js
@@ -0,0 +1,16 @@
+const seven = "1234567";
+let result = "#";
+for (let i = 0; i < seven.length; i++) {
+	console.log(result);
+	result = result + "#";
+}
+
+/*
+#
+##
+###
+####
+#####
+######
+#######
+*/
