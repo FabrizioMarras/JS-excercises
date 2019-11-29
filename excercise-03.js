9678260ec8c68034fb7c21d1140c8ac22c91a75f
Fabrizio Marras <37974948+FabrizioMarras@users.noreply.github.com>
2019-11-29 13:22:50 +0100

    Create excercise-03.js

---
+++ excercise-03.js
@@ -0,0 +1,7 @@
+/* 
+Write a program that create a string that represent a 8 x 8 grid using a new line character to separate lines.
+at each position of the grid there is either a "" (space) or a "#" character.
+
+Pass then the string to the console to output the chess board-like grid.
+*/
+
